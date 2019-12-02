8a865734a2982940ca160e10467829f2a7e0d85f
Wilson-Garnier <54800943+Wilson-Garnier@users.noreply.github.com>
2019-12-02 16:09:21 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -3,54 +3,11 @@
   header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
   header("Cache-Control: no-cache");
   header("Pragma: no-cache");
-  
-  try {
-    if (isset($_REQUEST['action'])) {
-      require "controleur.php";
-      require "controleurpage.php";
-      $employe = new Employes();
-      $page = new Pagination();
 
-      if ($_POST['action'] == 'Supprimer') {
-        $employe->setdelete(intval($_POST['ide']));
-      } 
+  if (isset($_REQUEST['action'])) {
 
-      if ($_POST['action'] == 'Ajouter') {
-        $employe->setAdd($_POST);
-      } 
-
-      if ($_POST['action'] == 'Modifier') {
-        $_POST['ide']=intval($_POST['ide']);
-        $employe->setUpdate($_POST);
-      } 
-
-      if ($_POST['action'] == 'Rechercher') {
-        $tblEmp = $employe->Search($_POST);
-        include "vue.php";
-      }
-
-      if ($_GET['action'] == 'Admin') {
-        header('Location: ./admin/index.php');  
-      }  
-
-      if ($_GET['action'] == 'Accueil') {
-        $tblEmp = $employe->getSelect();
-        include "vue.php";
-      }  
-    }
-    else {
-      require "controleur.php";
-      require "controleurpage.php";
-      $employes = new Employes();
-      $tblEmp = $employes->getSelect();
-      $pages = new Pagination();
-      $tbpag = $pages->getpage();
-      include "vue.php";
-    }
-
+    header('Location: ./vue.php'); 
   }
-  catch (Exception $e) {
-      erreur($e->getMessage());
-  }  
-  
-?>
+  else {
+    header('Location: ./vue.php'); 
+  }
--- vue.php
+++ vue.php
@@ -1,137 +1,48 @@
-<!doctype html>
-<html lang="fr" class="no-js">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Annuaire</title> 
-    <meta name="description" content="Annuaire des employés">
-    <link rel="stylesheet" href="annuaire.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../annuaire.css">
+    <style>
+        .champ{
+            margin-bottom: 10px;
+        }
+    </style>
+    <title>Document</title>
 </head>
 <body>
-  <header>
+<header>
     <div id="logo">ailTch</div>
       <!-- ------------------------------------------- Menu ---------------------------------------- -->
     <nav>  
         <ul>
-            <li><a href="index.php?action=Accueil"> Accueil</a></li>
-            <li><a href="index.php?action=Admin"> Administrateur</a></li>
+            <li><a href="../index.php?action=Accueil"> Accueil</a></li>
+            <li><a href="../index.php?action=Admin"> Statistiques</a></li>
         </ul> 
     </nav>
   </header>
   <section id="pageContent">
-  <article>
-    <br><h1> Annuaire des employés </h1>
-  </article>
     <article>
-      <!-- ------------------------------------------- Formulaire Recherche  ---------------------------------------- -->
-      <form action='index.php?action=Rechercher' method='POST'>
-        <table class="table_annuaire">
-          <tr> 
-            <td> <input type='text' name='id' id='id' disabled > </td>
-            <td> <input type='text' name='prenom' id='prenom' ></td>
-            <td> <input type='text' name='nom' id='nom' ></td> 
-            
-            <td colspan='2'> <input type='submit' name='action' value='Rechercher'></td>
-          </tr>
-        </table>
-      </form>
+        <h1> Annuaire des employés </h1>
     </article>
     <article>
-      <!-- ------------------------------------------- Formulaire accueil  ---------------------------------------- -->
-      <table class="table_annuaire">
-        <?php
-        echo "<thead>";
-          echo "<tr>";
-            echo "<th>#</th>";
-            echo "<th>Prénom</th>";
-            echo "<th>Nom</th>";
-            
-            echo "<th colspan='2'>Actions</th>";
-          echo "</tr>";
-        echo "</thead>";
-        
-        echo "<tbody>";
-          foreach ($tblEmp as $employe) {
-            echo "<form action='index.php?action=Accueil' method='POST'>";
-            echo 
-            "<tr>" 
-                ."<td>"."<input readonly type='number' name='ide' id='ide' value=".$employe['emp_id']."></td>" 
-                ."<td>"."<input type='text' name='prenom' id='prenom' value='".$employe['emp_pnom'] . "'></td>" 
-                ."<td>"."<input type='text' name='nom' id='nom' value='".$employe['emp_nom'] . "'></td>" 
-                
-                ."<td>". "<input type='submit' name='action' value='Supprimer'></td>"
-                ."<td>". "<input type='submit' name='action' value='Modifier'></td>".
-            "</tr>";
-            echo "</form>";
-          }
-        echo "</tbody>";
-        ?>
-      </table>
-      </article>
-      <article >
-            <!-- ------------------------------------------- Pagination ---------------------------------------- -->
-            <?php
-            
-           
-             
-            $debut = $limite* $page ;
-            echo "Affiche $limite sur $nombreelementtotal donnée(s) ";
-echo "<br/>";
-echo "page $page  sur $nombredepages ";
- 
-echo "<hr>";
-while ($element = $rqt->fetch()){
- 
-    echo $element['emp_nom'];
-    echo " - ";
-    echo $element['emp_pnom'];
-    echo "<br/>";
-}
- 
-echo "<hr>";
- 
-if($page<1){
- 
- } else{
- 
-    ?>
-    <a href="?page=<?php echo $page - 1; ?>">Page précèdent</a>&nbsp&nbsp
-     
-    <?php
-        }
-     
-    if ($page>$nombredepages-1){
-     
-    }else{
-    ?>
-    
-    
-    <a href="?page=<?php echo $page + 1; ?>">Page suivant</a>
-     
-    <?php
-    }
-    ?>
-     
- 
-
-      </article>
-      <!-- ------------------------------------------- Formulaire ajout ---------------------------------------- -->
-      <article>
-        <h1>Ajouter un employé</h1>
-        <br>
-        <form action="index.php?action=Accueil" method="POST">
-          <fieldset>
-            <input type="text" name="prenom" id="prenom"  placeholder="Prénom"> <br><br>
-            <input type="text" name="nom" id="nom"  placeholder="Nom"><br><br>
-            <input type="submit" name="action" value="Ajouter">
-            <br><br>
-          </fieldset>
+        <!-- ------------------------------------------- Formulaire Recherche  ---------------------------------------- -->
+        <h1>Administration</h1><br>
+        <form method="POST" action="index.php">
+            <div class="champ">
+                <label for="pseudo">Entrez un pseudonyme : </label>
+                <input type="text" id="pseudo" name="pseudo">
+            </div>
+            <div class="champ">
+                <label for="pass">Entrez un mot de passe :</label>
+                <input type="password" id="pass" name="pass">
+            </div>
+            <div class="champ">
+                <input type="submit" value="Envoyer">
+            </div>
         </form>
-      </article>
-  </section>
-  <footer>
-        <p>&copy; BTS SIO | Lycée Pierre Poivre | Version 4 </a>
-    </footer>
-</body>
-</html>
+    </article>
 </body>
 </html>
